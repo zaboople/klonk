3962a2993e7aab5f4df8c3ec8a8af0a596f4554b
zaboople <troymotte@yahoo.com>
2014-10-18 12:44:20 -0500

    More

--- java/org/tmotte/klonk/config/Boot.java
+++
@@ -1,9 +0,0 @@
-package org.tmotte.klonk.config;
-
-public class Boot {
-
-  public static void main(String[] args) {
-    BootContext.bootApplication(args);
-  }
-  
-}
--- test/test.sh
+++
@@ -1,7 +0,0 @@
-#Xshare:off is I think very important. Oh yeah let's share memory. No, thanks but really, fuck you.
-pwd
-ant config.test compile && java \
-    -Xshare:off \
-    -Xms6m \
-    -classpath build org.tmotte.klonk.config.Boot \
-    -home test/home "$@"
