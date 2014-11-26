d844c0ab570a69cab3741b082dc94be932110e6c
zaboople <troymotte@yahoo.com>
2014-11-26 17:06:37 -0600

    Fixed KAlert sizing

--- java/org/tmotte/klonk/windows/popup/KAlert.java
+++ java/org/tmotte/klonk/windows/popup/KAlert.java
@@ -19,6 +19,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
+import javax.swing.JEditorPane;
 import javax.swing.JWindow;
 import org.tmotte.common.swang.GridBug;
 import org.tmotte.common.swang.KeyMapper;
@@ -33,8 +34,7 @@ import org.tmotte.klonk.config.msg.Setter;
 public class KAlert implements Setter<String> {
 
   private JDialog win;
-  private JLabel msgLabel;
-  private JTextPane errorLabel;
+  private JEditorPane errorLabel;
   private JFrame parentFrame;
   private JButton ok;
   private Setter<Throwable> errorHandler;
@@ -63,20 +63,19 @@ public class KAlert implements Setter<String> {
   }
   public void show(String message) {
     init();
-    errorLabel.setSize(new Dimension(0,0));
     errorLabel.setPreferredSize(null);
+    errorLabel.setSize(new Dimension(100,100));
     errorLabel.setText(message);
-    win.pack();
-    int wide=errorLabel.getPreferredSize().width,
-        high=errorLabel.getSize().height;
-    if (wide>500){
-      double dwide=wide;
-      dwide/=500d;
-      int increment=(int)Math.round(dwide);
-      high=high*(increment+(increment> 5 ?2 :1));
-      errorLabel.setPreferredSize(new Dimension(500, high));
-      win.pack();
+    Dimension sized=errorLabel.getPreferredSize();
+    if (sized.height<100) {
+      errorLabel.setPreferredSize(null);
+      errorLabel.setText(message);
     }
+    else {
+      errorLabel.setSize(new Dimension(500,100));
+      errorLabel.setText(message);
+    }
+    win.pack();
     ok.requestFocusInWindow();
     show();
   }
@@ -101,9 +100,7 @@ public class KAlert implements Setter<String> {
   private void create() {
     win=new JDialog(parentFrame, true);
 
-    msgLabel=new JLabel();
-
-    errorLabel=new JTextPane();
+    errorLabel=new JEditorPane();
     errorLabel.setEditable(false); // as before
     errorLabel.setBorder(null);       
     errorLabel.setOpaque(false);
@@ -115,7 +112,6 @@ public class KAlert implements Setter<String> {
     gb.insets.left=10;gb.insets.right=10;
     gb.insets.top=10; gb.insets.bottom=10;
     gb.gridXY(0).weightXY(1);
-    gb.add(msgLabel);
     gb.fill=gb.BOTH;
     gb.addY(errorLabel);
     gb.weightXY(0);
@@ -123,7 +119,6 @@ public class KAlert implements Setter<String> {
     gb.insets.top=5;
     gb.insets.bottom=10;
     gb.addY(ok);
-    msgLabel.setVisible(false);
   }
   private void listen() {
     Action btnActions=new AbstractAction() {
@@ -140,12 +135,7 @@ public class KAlert implements Setter<String> {
   private void show(String message, Throwable e) {
     if (message==null)
       message="Whoops... ";
-    message=getAlert(message, e);
-    errorLabel.setPreferredSize(null);
-    errorLabel.setSize(new Dimension(0,0));
-    errorLabel.setText(message);
-    win.pack();
-    show();
+    show(getAlert(message, e));
   }
   private String getAlert(String message, Throwable e) {
     return message+"\n\n"+StackTracer.getStackTrace(e).replaceAll("\t", "    ");
@@ -169,20 +159,22 @@ public class KAlert implements Setter<String> {
           JFrame frame=PopupTestContext.makeMainFrame();
           KAlert ka=new KAlert(frame);
           try {throwTest();} catch (Exception f) {ka.fail(f);}
-          ka.show("This is simply the most pornographic thing anybody's seen in a while, in these parts."
-                 +" I'm not sure what kind of smut you folks are into but you won't be dissatisfied, I "
+          ka.show("This is simply the most weird thing anybody's seen in a while, in these parts."
+                 +" I'm not sure what you folks are into but you won't be dissatisfied, I "
                  +"assure you");
           ka.show(
             "I dislike you and everything and whatever and so on and howdy and man oh man am I tired "
            +"and this is going to be such a gigantically long sentence that it has no business fitting "
            +"in a window like this it's just too big and it goes on forever keep trying it's almost " 
            +"long enough ok now you're cooking just a little more lordy that's enough. Blah blah blah blah"
-           +"blah blah blah."
-           +"I dislike you and everything and whatever and so on and howdy and man oh man am I tired "
-           +"and this is going to be such a gigantically long sentence that it has no business fitting "
-           +"in a window like this it's just too big and it goes on forever keep trying it's almost " 
-           +"long enough ok now you're cooking just a little more lordy that's enough. Blah blah blah blah"
-           +"blah blah blah."
+           +"blah blah blah.\n\n"
+           +"Continuing with even more text about nothing in particular, yet something easily understood, "
+           +"or perhaps only in theory so, having been written by someone of questionable mental integrity "
+           +"and perspective who is not to be trusted with writing sentences of such length or really "
+           +"any sentences at all, not to mention sentences that could be misinterpreted as say, a declaration "
+           +"of war upon a sovereign nation, should the author happen to be president or secretary of state "
+           +"or some other high-ranking position in a heavily armed nation-state foolish enough to elect "
+           +"or appoint him. Let's hope that doesn't happen."
           );
           ka.show("I dislike you");
           ka.fail(new RuntimeException("I had a smoochy smooch"));
