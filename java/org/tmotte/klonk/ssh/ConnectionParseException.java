17738b944c554d4b678ad310d9ce3d851d01b310
zaboople <troymotte@yahoo.com>
2014-12-13 19:51:29 -0600

    Removed unnecessary class and tried to get to bottom of file open failures

--- java/org/tmotte/klonk/ssh/ConnectionParseException.java
+++
@@ -1,7 +0,0 @@
-package org.tmotte.klonk.ssh;
-/** FIXME is this garbage or what */
-public class ConnectionParseException extends Exception {
-  public ConnectionParseException(String msg){
-    super(msg);
-  }
-}
