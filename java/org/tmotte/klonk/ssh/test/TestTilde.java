e06fb238bed84941d761b377cebcc71b7e491e90
zaboople <troymotte@yahoo.com>
2014-12-27 14:56:46 -0600

    Cleaned up TestTilde and meh wanted to verify try-finally so

--- java/org/tmotte/klonk/ssh/test/TestTilde.java
+++ java/org/tmotte/klonk/ssh/test/TestTilde.java
@@ -7,14 +7,28 @@ import java.io.File;
 
 public class TestTilde {
   public static void main(String[] args) throws Exception {
+    testFinally();
+  }
+  
+  
+  private static void test(String[] args) throws Exception {
     SSHCommandLine cmd=new SSHCommandLine(args); 
     SSH ssh=cmd.ssh;
     System.out.println(ssh);
     System.out.println("BAM! ~ is: "+ssh.getTildeFix());
     ssh.close();
   }
-  private static void mylog(String msg) {
-    System.out.println("test.FilesListing: "+msg);
+  
+  
+  private static void testFinally() throws Exception {
+    try {
+      if (true)
+        throw new Exception("BANG");
+    } catch (Exception e) {
+      throw new RuntimeException("RETHROW FROM CATCH", e);
+    } finally {
+      System.out.println("NO MATTER WHAT");
+    }
   }
 
 }
