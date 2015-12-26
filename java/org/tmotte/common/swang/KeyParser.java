2d1f00f94abcd3e080ed4180f5e63403d7cce481
Troy Motte <troy.motte@civitaslearning.com>
2015-12-26 17:56:14 -0600

    Making the key parser even better

--- java/org/tmotte/common/swang/KeyParser.java
+++ java/org/tmotte/common/swang/KeyParser.java
@@ -4,28 +4,74 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.util.regex.Pattern;
 import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * This is just for mapping shortcut keys to and from a text representation.
  */
 public class KeyParser {
+  Map<Object, String> keyMap=new HashMap<>();
+  public KeyParser() {
+    for (Field field: KeyEvent.class.getFields())
+      try {
+        Object o=field.get(field);
+        String name=field.getName();
+        if (name.startsWith("VK_") && o instanceof Integer)
+          keyMap.put(o, name);
+      } catch (Exception e) {
+        throw new RuntimeException("Error processing field "+field.getName()+" "+field);
+      }
+  }
+  public String toString(KeyStroke ks) {
+    int mods=ks.getModifiers(), kc=ks.getKeyCode();
+    String keyName=keyMap.get(kc);
+    if (keyName==null)
+      throw new RuntimeException("Can't parse key code "+kc+" from "+ks);
+    String modName="";
+    if (mods==0)
+      modName="";
+    if ((mods & KeyEvent.CTRL_DOWN_MASK) != 0)
+      modName+=" ctrl";
+    if ((mods & KeyEvent.META_DOWN_MASK) != 0)
+      modName+=" command";
+    if ((mods & KeyEvent.ALT_DOWN_MASK) != 0)
+      modName+=" alt/option";
+    if ((mods & KeyEvent.SHIFT_DOWN_MASK) != 0)
+      modName+=" shift";
+    return (modName+" "+keyName).trim();
+  }
+
+
   private static Pattern dividerRegex=Pattern.compile("(-| )");
 
   public static KeyStroke key(String parse) throws Exception {
-    int mods=0;
-    int hotkey=0;
-    for (String next : dividerRegex.split(parse)) {
-      next=next.trim();
-      if (!next.equals("")) {
-        int mod=getMod(next);
-        if (mod!=0)
-          mods|=mod;
-        else
-          hotkey=getKey(next);
+    try {
+      int mods=0;
+      int hotkey=0;
+      for (String next : dividerRegex.split(parse)) {
+        next=next.trim();
+        if (!next.equals("")) {
+          int mod=getMod(next);
+          if (mod!=0)
+            mods|=mod;
+          else
+          if (hotkey!=0)
+            throw new RuntimeException("More than two keyboard characters specified (multiple modifiers are OK), cannot accept: "+next);
+          else
+            hotkey=getKey(next);
+        }
       }
+      return KeyMapper.key(hotkey, mods);
+    } catch (Exception e) {
+      throw new RuntimeException(
+        "Failed to parse \""+parse+"\", original error was: "+e,
+        e
+      );
     }
-    return KeyMapper.key(hotkey, mods);
   }
+
+
   private static int getMod(String s) {
     String modName=s.toLowerCase();
     int downMask=modName.indexOf("_down_mask");
@@ -37,7 +83,7 @@ public class KeyParser {
     if (modName.equals("command") || modName.equals("cmd") || modName.equals("meta"))
       return KeyEvent.META_DOWN_MASK;
     else
-    if (modName.equals("option") || modName.equals("alt") || modName.equals("opt"))
+    if (modName.equals("option") || modName.equals("alt") || modName.equals("opt") || modName.equals("alt/option"))
       //Not a bug - on mac, option is alt. Don't ask me.
       return KeyEvent.ALT_DOWN_MASK;
     else
@@ -65,7 +111,12 @@ public class KeyParser {
     }
   }
   public static void main(String[] args) throws Exception {
-    for (String arg: args)
-      System.out.println("Key: "+key(arg));
+    KeyParser parser=new KeyParser();
+    for (String arg: args) {
+      KeyStroke parsed=key(arg);
+      String reversed=parser.toString(parsed);
+      KeyStroke parsedAgain=key(reversed);
+      System.out.println("From: \""+arg+"\"\n    to: "+parsed+"\n  to: \""+reversed+"\"\n    to: "+parsedAgain);
+    }
   }
 }
