4b58d73d5a430299302168034489d13172f4d56a
Troy Motte <troy.motte@civitaslearning.com>
2016-01-10 15:52:33 -0600

    Windows build info

--- lib/makewin.bat
+++ lib/makewin.bat
@@ -1,6 +1,6 @@
-# Note: javapackager comes with java 8 distributions only
-# This will advise you to install software from http://www.jrsoftware.org/, which
-# is kind of lame.
-#
+rem Note: javapackager comes with java 8 distributions only
+rem This will advise you to install software from http://www.jrsoftware.org/, which
+rem is kind of lame.
+
 ant clean config.prod jar
 javapackager -deploy -native exe -srcfiles dist/klonk.jar -outdir dist/ -outfile klonk -appclass org.tmotte.klonk.config.Klonk -title "Klonk" -name Klonk
