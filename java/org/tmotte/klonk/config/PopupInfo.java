38463c77e5606c9e557cbb58a2ab1dc0a556e0d7
zaboople <troymotte@yahoo.com>
2016-02-06 17:54:07 -0600

    Making it where PopupInfo can listen for font changes

--- java/org/tmotte/klonk/config/PopupInfo.java
+++ java/org/tmotte/klonk/config/PopupInfo.java
@@ -1,14 +1,25 @@
 package org.tmotte.klonk.config;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JFrame;
-import org.tmotte.klonk.config.option.FontOptions;
 import org.tmotte.common.swang.CurrentOS;
+import org.tmotte.klonk.config.option.FontOptions;
+import org.tmotte.klonk.config.msg.Setter;
 
 public class PopupInfo {
   public final JFrame parentFrame;
   public final CurrentOS currentOS;
+  private final List<Setter<FontOptions>> fontListeners=new java.util.ArrayList<>(30);
 
   public PopupInfo(JFrame parentFrame, CurrentOS currentOS) {
     this.parentFrame=parentFrame;
     this.currentOS=currentOS;
   }
+  public void addFontListener(Setter<FontOptions> listener) {
+    fontListeners.add(listener);
+  }
+  public List<Setter<FontOptions>> getFontListeners() {
+    return fontListeners;
+  }
+
 }
